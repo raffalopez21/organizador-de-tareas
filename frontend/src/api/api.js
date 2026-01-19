8e92d04319c4a88cc128a162ade964b77ed4d770
raffalopez <raffalopez@gmail.com>
2026-01-18 23:17:45 -0300

    modifica api_url

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,5 +1,5 @@
 // api.js - Usar rutas relativas (sin /api)
-const API_URL = "";  // Rutas relativas al frontend
+const API_URL = "https://organizador-de-tareas-hgpd.onrender.com/api";
 
 // Obtener tareas
 export const getTareas = async () => {

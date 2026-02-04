b6496a7218b22172d82eb0f6f615adb94c82cfeb
raffalopez <raffalopez@gmail.com>
2026-02-04 01:11:58 -0300

    mas nueva interfaz 2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import { Background } from './components/Background';
 import { TaskInput } from './components/TaskInput';
